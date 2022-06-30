d06e4b4a3860f9f00e7101e995ab021c8aa81f23
huseyin <huseyinsarsilmaz@hotmail.com>
2022-06-30 15:46:31 +0300

    Add twitter logo

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,14 @@
+import Image from "next/image";
+
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className="" >
+        <Image 
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png "
+          width="50" height="50"
+        ></Image>
+      </div>
+    </div>
   )
 }
